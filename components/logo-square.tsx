e41628a2a478386e358aa09be4fdc01699e959fb
dhatuorganics <dhatuorganics@dhatu.org>
2026-04-07 17:27:05 +0530

    Fix logo - use SVG for opengraph, PNG for navbar display

--- components/logo-square.tsx
+++ components/logo-square.tsx
@@ -1,21 +1,23 @@
 import clsx from "clsx";
-import LogoIcon from "./icons/logo";
 
 export default function LogoSquare({ size }: { size?: "sm" | undefined }) {
   return (
     <div
       className={clsx(
-        "flex flex-none items-center justify-center border border-neutral-200 bg-white dark:border-neutral-700 dark:bg-black",
+        "flex flex-none items-center justify-center overflow-hidden rounded-xl border border-neutral-200 bg-white dark:border-neutral-700 dark:bg-black",
         {
           "h-[40px] w-[40px] rounded-xl": !size,
           "h-[30px] w-[30px] rounded-lg": size === "sm",
         },
       )}
     >
-      <LogoIcon
-        className={clsx({
-          "h-[16px] w-[16px]": !size,
-          "h-[10px] w-[10px]": size === "sm",
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
+        src="/logo.png"
+        alt="Logo"
+        className={clsx("object-contain", {
+          "h-[36px] w-[36px]": !size,
+          "h-[26px] w-[26px]": size === "sm",
         })}
       />
     </div>
